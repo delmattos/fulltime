1b5d5095fdc2cec4fce41fa06675071da51720b8
Delmas Mattos <delmasmattos@gmail.com>
2016-10-03 12:12:51 -0300

    Added input validation in email and password fields on login screen

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,4 +4,13 @@ $(function() {
   $('#lunch-return').timepicki();
   $('#exit-time').timepicki();
   $('[data-toggle="tooltip"]').tooltip();
-})
+});
+
+$(function () {
+  $('#login-form').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+  })
+  .on('form:submit', function() {
+    return true;
+  });
+});
