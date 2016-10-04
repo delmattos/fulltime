d3ac7982e90f3d64cf062131f1c52122661a2927
Delmas Mattos <delmasmattos@gmail.com>
2016-10-03 23:28:16 -0300

    Improvements in input fieds validation on login screen

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,12 +5,3 @@ $(function() {
   $('#exit-time').timepicki();
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-$(function () {
-  $('#login-form').parsley().on('field:validated', function() {
-    var ok = $('.parsley-error').length === 0;
-  })
-  .on('form:submit', function() {
-    return true;
-  });
-});
