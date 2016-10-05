b2048cf03ab45523593c56e879d61abfd178e219
Delmas Mattos <delmasmattos@gmail.com>
2016-10-05 15:17:58 -0300

    Implemented the possibility of editing past hours in screen day.html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,3 +21,60 @@ $('#password').keypress(function (e) {
   e.preventDefault();
   return false;
 });
+
+$('#edit-start').on('click', function() {
+  $('#start-time').prop('disabled',false);
+  $('#edit-start').css('display','none');
+  $('#delete-start').css('display','none');
+  $('#save-start').css('display','inline-block');
+});
+
+$('#save-start').on('click', function() {
+  $('#start-time').prop('disabled',true);
+  $('#edit-start').css('display','inline-block');
+  $('#delete-start').css('display','inline-block');
+  $('#save-start').css('display','none');
+});
+
+$('#edit-lunch').on('click', function() {
+  $('#lunch-time').prop('disabled',false);
+  $('#edit-lunch').css('display','none');
+  $('#delete-lunch').css('display','none');
+  $('#save-lunch').css('display','inline-block');
+});
+
+$('#save-lunch').on('click', function() {
+  $('#lunch-time').prop('disabled',true);
+  $('#edit-lunch').css('display','inline-block');
+  $('#delete-lunch').css('display','inline-block');
+  $('#save-lunch').css('display','none');
+});
+
+$('#edit-return').on('click', function() {
+  $('#lunch-return').prop('disabled',false);
+  $('#edit-return').css('display','none');
+  $('#delete-return').css('display','none');
+  $('#save-return').css('display','inline-block');
+});
+
+$('#save-return').on('click', function() {
+  $('#lunch-return').prop('disabled',true);
+  $('#edit-return').css('display','inline-block');
+  $('#delete-return').css('display','inline-block');
+  $('#save-return').css('display','none');
+});
+
+$('#edit-exit').on('click', function() {
+  $('#exit-time').prop('disabled',false);
+  $('#edit-exit').css('display','none');
+  $('#delete-exit').css('display','none');
+  $('#save-exit').css('display','inline-block');
+});
+
+$('#save-exit').on('click', function() {
+  $('#exit-time').prop('disabled',true);
+  $('#edit-exit').css('display','inline-block');
+  $('#delete-exit').css('display','inline-block');
+  $('#save-exit').css('display','none');
+});
+
