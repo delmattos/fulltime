558453cb2d70ad624898ed95be6179d5365bc949
Delmas Mattos <delmasmattos@gmail.com>
2018-01-26 20:39:55 -0200

    The return of the jedi

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,6 @@
 $('#password').keypress(function (e) {
   var regex = new RegExp("^[a-zA-Z0-9]+$");
   var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
-  if (regex.test(str)) {
-      return true;
-  }
-  e.preventDefault();
-  return false;
 });
 
 $(function() {
--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
